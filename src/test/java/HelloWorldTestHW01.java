65a634b9b111edc4ad9044475cc8b39761083be1
Lunatik06 <36671690+Lunatik06@users.noreply.github.com>
2022-12-03 21:22:32 +0300

    HelloWorldTest

---
+++ src/test/java/HelloWorldTestHW01.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTestHW01 {
+
+    @Test
+    public void testHelloWorldhw01(){
+        System.out.println("Hello from Alexey");
+    }
+}
