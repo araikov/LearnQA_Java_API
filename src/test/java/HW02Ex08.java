5c066b9387544adee898a4002c914bda9b0edc40
Lunatik06 <36671690+Lunatik06@users.noreply.github.com>
2022-12-11 20:35:30 +0300

    HW02 - EX08

---
+++ src/test/java/HW02Ex08.java
@@ -0,0 +1,50 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HW02Ex08 {
+
+    @Test
+    public void testHelloWorld() {
+
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        String token = response.getString("token");
+        int seconds = response.getInt("seconds");
+        System.out.println(token);
+        System.out.println(seconds);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("token",token);
+
+        Response responseTokenNotReady = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        responseTokenNotReady.print();
+
+        try {
+        Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Response responseToken = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        responseToken.print();
+    }
+}
